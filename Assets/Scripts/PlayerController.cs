bc61e20e64003737397ab0257ae254e4347036a9
Jeff Hube <jeff2365@gmail.com>
2019-01-25 21:15:18 -0500

    Basic player movement

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private const float SPEED = 5.0f;
+    private const float GROUND_RADIUS = 0.1f;
+    private const float JUMP_VELOCITY = 6;
+
+    private Rigidbody2D _rigidbody;
+
+    public Transform GroundCheck;
+
+    public LayerMask WhatIsGround;
+
+    void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+    }
+
+    private void FixedUpdate()
+    {
+        bool grounded = null != Physics2D.OverlapCircle(GroundCheck.position, GROUND_RADIUS, WhatIsGround);
+
+        Vector2 newVelocity = new Vector2(Input.GetAxis("Horizontal") * SPEED, _rigidbody.velocity.y);
+
+        if (grounded && newVelocity.y <= 0 && Input.GetButton("Jump"))
+        {
+            newVelocity.y += JUMP_VELOCITY;
+        }
+
+        _rigidbody.velocity = newVelocity;
+    }
+}

bcc9a543fca51f4de549095980f41872f773d000
Emily <emilyhorton1994@gmail.com>
2019-01-25 21:30:16 -0500

    Really basic pause, attach to any controller object. Tab to toggle pause.

---
+++ Assets/Scripts/PauseButton.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseButton : MonoBehaviour {
+
+	void Update ()
+    {
+        
+		//Calls togglepause if key is pressed
+        if (Input.GetKeyDown("tab"))
+        {
+            TogglePause();
+        }
+	}
+	
+	// Update is called once per frame
+	public void TogglePause ()
+    {
+
+        //Changes game speed
+        if (Time.timeScale ==1)
+        {
+            Time.timeScale = 0;
+        }
+        else
+        {
+            Time.timeScale = 1;
+        }
+	}
+}

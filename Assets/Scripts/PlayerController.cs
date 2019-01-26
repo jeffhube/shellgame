458553f7368b5501ac71d9f0206d2a2fb73fa94e
Jeff Hube <jeff2365@gmail.com>
2019-01-25 23:25:45 -0500

    Add shell pickup / drop with e

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -9,18 +9,61 @@ public class PlayerController : MonoBehaviour
     private const float JUMP_VELOCITY = 6;
 
     private Rigidbody2D _rigidbody;
+    private BoxCollider2D _boxCollider;
 
     public Transform GroundCheck;
+    public Transform ShellSocket;
+    public SpriteRenderer ShellSpriteRenderer;
+    public GameObject ShellPrefab;
+    public Shell.ShellType ShellType;
 
     public LayerMask WhatIsGround;
+    public LayerMask ShellLayer;
 
     void Start()
     {
         _rigidbody = GetComponent<Rigidbody2D>();
+        _boxCollider = GetComponent<BoxCollider2D>();
     }
 
     void Update()
     {
+        if (Input.GetButtonDown("Use"))
+        {
+            if (ShellType == Shell.ShellType.None)
+            {
+                Collider2D collider = Physics2D.OverlapBox(transform.position, _boxCollider.size, 0, ShellLayer);
+                if (collider != null)
+                {
+                    GameObject shellObject = collider.gameObject;
+                    SpriteRenderer spriteRenderer = shellObject.GetComponent<SpriteRenderer>();
+                    Shell shell = shellObject.GetComponent<Shell>();
+
+                    ShellType = shell.Type;
+                    ShellSpriteRenderer.sprite = spriteRenderer.sprite;
+
+                    Destroy(shellObject);
+                }
+            }
+            else
+            {
+                GameObject shellObject = Instantiate(ShellPrefab);
+                shellObject.transform.position = ShellSocket.position;
+
+                SpriteRenderer spriteRenderer = shellObject.GetComponent<SpriteRenderer>();
+                spriteRenderer.sprite = ShellSpriteRenderer.sprite;
+                Shell shell = shellObject.GetComponent<Shell>();
+                shell.Type = ShellType;
+
+                ShellType = Shell.ShellType.None;
+                ShellSpriteRenderer.sprite = null;
+            }
+        }
+    }
+
+    private void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 200, 40), this.ShellType.ToString());
     }
 
     private void FixedUpdate()

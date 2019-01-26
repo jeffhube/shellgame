c56a5338c9d2e342d6734fbebada1f5029dc40ca
bkalb <bkalb@blanketfortgames.com>
2019-01-25 22:12:05 -0500

    Breakables

---
+++ Assets/Scripts/Breakable.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Breakable : MonoBehaviour {
+    public void Break()
+    {
+        GetComponent<Renderer>().enabled = false;
+        GetComponent<Collider2D>().enabled = false;
+        GetComponent<ParticleSystem>().Play();
+        Destroy(gameObject, 5000);
+    }
+}

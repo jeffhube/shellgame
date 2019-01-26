25ab79227763339e3b2d8425e99e461c94cf9ddb
Emily <emilyhorton1994@gmail.com>
2019-01-26 12:46:12 -0500

    Level Select Added

---
+++ Assets/Scripts/LevelSelect.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelSelect : MonoBehaviour {
+
+	// Loads the specified levelname
+	public void LoadLevel (string levelName)
+    {
+        SceneManager.LoadScene(levelName);
+	}
+
+    public void SomeMethod()
+    {
+
+    }
+}

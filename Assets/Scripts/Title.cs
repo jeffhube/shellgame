4e5a8c39f13a38f461bb4a838f1451172d376ef9
Emily <emilyhorton1994@gmail.com>
2019-01-26 19:51:18 -0500

    Shell artwork, title screen update

---
+++ Assets/Scripts/Title.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Title : MonoBehaviour {
+	
+	// Advance to next screen on anykey
+	void Update ()
+    {
+        if (Input.anyKey)
+        {
+            SceneManager.LoadScene("LevelSelect");
+        }
+		
+	}
+}

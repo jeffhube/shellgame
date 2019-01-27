8caf6d142fd26660b3fa4474315501f8f35d0203
bkalb <bkalb@blanketfortgames.com>
2019-01-27 14:47:17 -0500

    Quit with Esc from level select

--- Assets/Scripts/LevelSelect.cs
+++ Assets/Scripts/LevelSelect.cs
@@ -3,6 +3,14 @@ using UnityEngine.SceneManagement;
 
 public class LevelSelect : MonoBehaviour {
 
+    void Update()
+    {
+        if (Input.GetButtonDown("Cancel"))
+        {
+            Application.Quit();
+        }
+    }
+
 	// Loads the specified levelname
 	public void LoadLevel (string levelName)
     {
